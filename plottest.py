f9faf837dcf1f152b4af52427e3c4570b6a4bb25
laurathepluralized <laurathepluralized@gmail.com>
2013-11-26 18:00:06 -0500

    uncommented HMM function

--- plottest.py
+++ plottest.py
@@ -6,7 +6,7 @@ import numpy as np
 from datetime import datetime
 import os
 import matplotlib.pyplot as pyplot
-#from sklearn.hmm import GaussianHMM
+from sklearn.hmm import GaussianHMM
 
 def dateconv(date_str):
     date = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
@@ -20,41 +20,53 @@ def t1dmread(file_name):
     data = np.loadtxt(file_name, delimiter=',', skiprows=1,converters = { 0 : dateconv },usecols=(0,7,10,13,17,18,19,20,21,22,23,24,25,26,27), dtype=dtypes)
     return data
 
-#def basichmm(datacol1,datacol2): #This may need more arguments if we discover correlation with multiple factors.
-#    cols = np.column_stack([datacol1,datacol2])
-#    
-#    components = 7 # 7 components = 7 stages?
-#    model = GaussianHMM(components, covariance_type="diag", n_iter=1000)
-#    model.fit([cols])
-#    hidden_states = model.predict(cols)
-#    print("Hidden States:")
-#    for i in range(components):
-#        print("Hidden state %d" % i)
-#        print("mean = ", model.means_[i])
-#        print("variance = %d", np.diag(model.covars_[i]))
-#        print()
-#        
-#    return 
+def basichmm(datacol1,datacol2): #This may need more arguments if we discover correlation with multiple factors.
+    cols = np.column_stack([datacol1,datacol2])
+    
+    components = 7 # 7 components = 7 stages?
+    model = GaussianHMM(components, covariance_type="diag", n_iter=1000)
+    model.fit([cols])
+    hidden_states = model.predict(cols)
+    print("Hidden States:")
+    for i in range(components):
+        print("Hidden state %d" % i)
+        print("mean = ", model.means_[i])
+        print("variance = %d", np.diag(model.covars_[i]))
+        print()
+        
+    return 
 
 
-def stuffPlot(timestamps,values):
+def stuffPlot(timestamps,func,title,ylabel):
     fig = pyplot.figure()
-    pyplot.title('Sensor Data')
-    pyplot.ylabel('Sensor Data')
-    pyplot.xlabel('Timestamp')
-    pyplot.plot(timestamps,values,marker='o')
+    pyplot.title(title)
+    pyplot.ylabel(ylabel)
+    pyplot.xlabel('Time')
+    pyplot.plot(timestamps,func,marker='o')
+
+    
     return fig
 
 #--------------------------------------------------
+
+# If no plots folder exists, make a folder to store all of the plots
+if not os.path.exists('/Users/lauraS/Dropbox/aaaaGATech/aaasem1/ai/miniproject2/src/cyborg-t1dm/plots'):
+    os.mkdir('/Users/lauraS/Dropbox/aaaaGATech/aaasem1/ai/miniproject2/src/cyborg-t1dm/plots')
+
+pyplot.ion()
+
 
 data = t1dmread('/Users/lauraS/Dropbox/aaaaGATech/aaasem1/ai/miniproject2/src/cyborg-t1dm/trimmedDataFiles/MYFILE101.no_gaps_trimmed.csv')
 timestamps101 = data['timestamp']
 skinTemp101 = data['skin temp']
 airTemp101 = data['air temp']
 steps101 = data['steps']
+hr101 = data['hr']
+cgm101 = data['cgm']
+normskintemp101 = skinTemp101 - airTemp101
 
-if not os.path.exists('/Users/lauraS/Dropbox/aaaaGATech/aaasem1/ai/miniproject2/src/cyborg-t1dm/plots'):
-    os.mkdir('/Users/lauraS/Dropbox/aaaaGATech/aaasem1/ai/miniproject2/src/cyborg-t1dm/plots')
 
-fig = stuffPlot(timestamps101,skinTemp101)
-fig.savefig('/Users/lauraS/Dropbox/aaaaGATech/aaasem1/ai/miniproject2/src/cyborg-t1dm/plots/skintemp101')
+
+fig = stuffPlot(timestamps101,(skinTemp101-airTemp101),'Skin Temp - Near Temp, Subject 101','Skin Temp - Near Temp')
+
+fig.savefig('/Users/lauraS/Dropbox/aaaaGATech/aaasem1/ai/miniproject2/src/cyborg-t1dm/plots/skinneartemp101')
